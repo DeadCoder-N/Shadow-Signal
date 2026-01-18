61deb2041b7f354f80c76e68fda6edf15d2d47f8
Dead Coderr <niteshsawardekar972@gmail.com>
2026-01-18 07:38:18 +0530

    Restore game interface with cache bust

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App-test.tsx";
+import App from "./App.tsx";
 
+// Force rebuild timestamp: 1735659000
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
