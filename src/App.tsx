cc4ecaf5e64fd16b5c889906ed371600ed8fd858
Dead Coderr <niteshsawardekar972@gmail.com>
2026-01-18 07:27:08 +0530

    0.006

--- src/App.tsx
+++ src/App.tsx
@@ -4,22 +4,13 @@ import { Lobby } from './components/game/Lobby';
 import { GameRoom } from './components/game/GameRoom';
 
 function App() {
-  try {
-    const { room } = useGameStore();
+  const { room } = useGameStore();
 
-    return (
-      <div className="min-h-screen bg-gray-900">
-        {!room ? <Lobby /> : <GameRoom />}
-      </div>
-    );
-  } catch (error) {
-    return (
-      <div className="min-h-screen bg-red-900 text-white p-8">
-        <h1>Error Loading App</h1>
-        <p>{error?.toString()}</p>
-      </div>
-    );
-  }
+  return (
+    <div className="min-h-screen bg-gray-900">
+      {!room ? <Lobby /> : <GameRoom />}
+    </div>
+  );
 }
 
 export default App;
