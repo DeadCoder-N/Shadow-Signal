1280d28b2d7b4f9c3d71db32ef985ec62ae571a9
Dead Coderr <niteshsawardekar972@gmail.com>
2026-01-18 07:23:31 +0530

    0.004

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,25 @@
 import React from 'react';
-import { SimpleTest } from './components/SimpleTest';
+import { useGameStore } from './store/gameStore';
+import { Lobby } from './components/game/Lobby';
+import { GameRoom } from './components/game/GameRoom';
 
 function App() {
-  // Temporarily use simple test component
-  return <SimpleTest />;
+  try {
+    const { room } = useGameStore();
+
+    return (
+      <div className="min-h-screen bg-gray-900">
+        {!room ? <Lobby /> : <GameRoom />}
+      </div>
+    );
+  } catch (error) {
+    return (
+      <div className="min-h-screen bg-red-900 text-white p-8">
+        <h1>Error Loading App</h1>
+        <p>{error?.toString()}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
