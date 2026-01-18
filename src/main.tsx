435f467eaf0be12a25875a4a2fe6998cc10588bb
Dead Coderr <niteshsawardekar972@gmail.com>
2026-01-18 07:41:49 +0530

    FINAL FIX: Direct lobby render

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import App from "./App.tsx";
+import App from "./AppNew.tsx";
 
-// Force rebuild timestamp: 1735659000
+// Force rebuild: 1735659100
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
